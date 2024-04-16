c44b819af1a9904ca0e5bc1972bf36894521e1cf
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2024-04-16 22:42:01 +0530

    Add files via upload

---
+++ Avatu.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void Display(int iRow, int iColumn)
+{
+	
+	int i=0, j=0;
+
+
+}
+
+int main()
+{
+	int iRow=0;
+	int iColumn=0;
+
+	printf("Enter the Row number:\n");
+	scanf("%d",&iRow);
+
+	printf("Enter the Column:\n");
+	scanf("%d",&iColumn);
+
+	Display(iRow, iColumn);
+
+	return 0;
+}
