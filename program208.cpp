a5a6984d63b5c5bf6c66c3f9226711e32cb57924
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2025-10-09 10:46:48 +0530

    program208.cpp

--- program208.cpp
+++ program208.cpp
@@ -9,7 +9,7 @@ class Number
         int iNo2;      
     
         Number(int X, int Y)          
-        {     
+        {      
             iNo1 = X;       
             iNo2 = Y;        
         }        
